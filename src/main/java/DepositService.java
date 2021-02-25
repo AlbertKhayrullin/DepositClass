ce6c11b4ee556eec8d7732404e58be71b129b66b
AlbertKhayrullin <albertKh1620@rambler.ru>
2021-02-25 13:30:34 +0300

    fixed #1

--- src/main/java/DepositService.java
+++ src/main/java/DepositService.java
@@ -1,6 +1,6 @@
 public class DepositService {
     double calculate(int deposit, double percent, int replenishment, int days, int replenishmentDays) {
-        double incom = deposit * days * percent / 36500 + replenishment * replenishmentDays * percent / 36500;
+        double incom = deposit * days / 365 * percent / 100 + replenishment * replenishmentDays / 365 * percent / 100;
         double scale = Math.pow(10, 2);
         double result = Math.ceil(incom * scale) / scale;
         return result;
